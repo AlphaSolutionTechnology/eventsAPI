975dd2684e966f0218dfa6a508e9369acdf27a28
Andre-PI <andrelucas_PI@proton.me>
2024-12-18 23:29:55 -0300

    consertando a base do spring project

---
+++ src/test/java/com/alphasolutions/eventapi/EventApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphasolutions.eventapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
