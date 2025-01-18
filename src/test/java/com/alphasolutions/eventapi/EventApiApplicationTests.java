bce6c155010d800188150c49e31482ae8728abff
Andre-PI <andrelucas_PI@proton.me>
2025-01-18 10:46:33 -0300

    adição do vertexAI

--- src/test/java/com/alphasolutions/eventapi/EventApiApplicationTests.java
+++ src/test/java/com/alphasolutions/eventapi/EventApiApplicationTests.java
@@ -1,13 +1,17 @@
 package com.alphasolutions.eventapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.autoconfigure.vertexai.gemini.VertexAiGeminiAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
+@EnableAutoConfiguration(exclude = VertexAiGeminiAutoConfiguration.class)
 class EventApiApplicationTests {
 
     @Test
     void contextLoads() {
+        // Teste básico
     }
-
 }
