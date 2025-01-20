0e42498b030a9837bc78f3bdace5f73a4744ad36
pvdias0 <p.vitordias@outlook.com>
2025-01-20 19:11:57 -0300

    criacao do PalestraController

---
+++ src/main/java/com/alphasolutions/eventapi/controller/PalestraController.java
@@ -0,0 +1,41 @@
+package com.alphasolutions.eventapi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alphasolutions.eventapi.model.Palestra;
+import com.alphasolutions.eventapi.repository.PalestraRepository;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
+
+@RestController
+@RequestMapping("/api/palestra")
+public class PalestraController {
+    
+private PalestraRepository palestraRepository;
+
+
+@GetMapping("/lista")
+public List<Palestra> PalestraList(@PathVariable Long eventoId){
+    return palestraRepository.findAll();
+}
+
+
+@PostMapping("/criar")
+public Palestra createPalestra(@RequestBody Palestra palestra){
+    return palestraRepository.save(palestra);
+}
+
+
+
+}
