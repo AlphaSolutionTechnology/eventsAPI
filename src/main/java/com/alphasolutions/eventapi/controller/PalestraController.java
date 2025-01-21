c0ced016f8767029bc907c9fc38fff6737fdf941
pvdias0 <p.vitordias@outlook.com>
2025-01-21 03:26:30 -0300

    endpoints de palestra

--- src/main/java/com/alphasolutions/eventapi/controller/PalestraController.java
+++ src/main/java/com/alphasolutions/eventapi/controller/PalestraController.java
@@ -1,14 +1,18 @@
 package com.alphasolutions.eventapi.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.DeleteExchange;
 
 import com.alphasolutions.eventapi.model.Palestra;
+import com.alphasolutions.eventapi.model.PalestraIdsDTO;
 import com.alphasolutions.eventapi.repository.PalestraRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,5 +49,35 @@ public ResponseEntity<?> createPalestra(@RequestBody Palestra palestra) {
 }
 
 
+@DeleteMapping("/excluir")
+public ResponseEntity<?> deletePalestras(@RequestBody PalestraIdsDTO dto) {
+    List<Long> ids = dto.getIds();
+
+    if (ids.isEmpty()) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Nenhuma palestra selecionada para exclusão.");
+    }
+
+    List<Long> idsNaoEncontrados = new ArrayList<>();
+    for (Long id : ids) {
+        if (palestraRepository.existsById(id)) {
+            palestraRepository.deleteById(id);
+        } else {
+            idsNaoEncontrados.add(id);
+        }
+    }
+
+    if (!idsNaoEncontrados.isEmpty()) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("As palestras com os seguintes IDs não foram encontradas: " + idsNaoEncontrados);
+    }
+
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build(); 
+}
+
+
+
 
 }
+
+
---
+++ src/main/java/com/alphasolutions/eventapi/model/PalestraIdsDTO.java
@@ -0,0 +1,15 @@
+package com.alphasolutions.eventapi.model;
+
+import java.util.List;
+
+public class PalestraIdsDTO {
+    private List<Long> ids;
+
+    public List<Long> getIds() {
+        return ids;
+    }
+
+    public void setIds(List<Long> ids) {
+        this.ids = ids;
+    }
+    }
