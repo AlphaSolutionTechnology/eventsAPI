4322148b781cd64ff6e81a43f5e88cfa41a04ff5
Andre-PI <andrelucas_PI@proton.me>
2025-02-08 11:46:03 -0300

    bugfix

--- src/main/java/com/alphasolutions/eventapi/repository/RankingViewRepository.java
+++ src/main/java/com/alphasolutions/eventapi/repository/RankingViewRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface RankingViewRepository extends JpaRepository<RankingView, Long> {
-    List<RankingView> findByIdEvento(String eventId);
+    List<RankingView> findAllByIdEvento(Long idEvento);
 }
