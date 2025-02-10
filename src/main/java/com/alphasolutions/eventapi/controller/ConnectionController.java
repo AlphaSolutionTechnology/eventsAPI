749acddc1ce393054add21945161f0bcbdab5a18
Andre-PI <andrelucas_PI@proton.me>
2025-02-10 12:08:18 -0300

    adição de novas rotas

--- src/main/java/com/alphasolutions/eventapi/controller/ConnectionController.java
+++ src/main/java/com/alphasolutions/eventapi/controller/ConnectionController.java
@@ -35,13 +35,12 @@ public class ConnectionController {
             List<Conexao> conexao = connectionServiceImpl.getThisUserConnectionRequest(userId);
             List<ConexaoDTO> connectionDto = new ArrayList<>(conexao.size());
             for (Conexao con : conexao) {
-                System.out.println(con.toString());
                 connectionDto.add(new ConexaoDTO(con.getSolicitante().getUniqueCode(),con.getSolicitante().getNome()));
             }
 
             return ResponseEntity.status(HttpStatus.OK).body(Map.of("server",connectionDto));
         }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("server","Invalid token"));
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(Map.of("server","Invalid token"));
     }
 
 
@@ -49,13 +48,12 @@ public class ConnectionController {
     public ResponseEntity<String> answerConnectionRequest(@CookieValue(value = "eventToken") String token, @RequestBody AlphaConnectionRequest connectionRequest) {
         Map<String,Object> validatedToken = jwtUtil.extractClaim(token);
         if (validatedToken.get("error") == null) {
-            System.out.println(validatedToken.get("unique_code"));
             if(validatedToken.get("unique_code").equals(connectionRequest.getTo())){
                 connectionServiceImpl.answerConnectionRequest(connectionRequest.getTo(),connectionRequest.getFrom(),connectionRequest.getStatus());
                 return ResponseEntity.ok("Connection request " + connectionRequest.getStatus() + " successfully");
             }
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Unauthorized");
         }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid token");
     }
 }
